aa5f93f5d19176f0ba07162c0577988a1582100b
mathieu <mathieu@T40.pouet>
2015-05-31 10:34:07 +0200

    ?

--- utilisation/tweetedit/hash.sh
+++ utilisation/tweetedit/hash.sh
@@ -1,2 +1,7 @@
-sed -i "s/#.\w*/<balise>&<\/balise>/g" tweets.html
-sed -i "s/@.\w*/<balise>&<\/balise>/g" tweets.html
+#suppression des guillemets offerts gracieusement par google
+sed -i 's/twitter_user">\t"/twitter_user">/g' tweets.html
+sed -i 's/\."/\./g' tweets.html
+
+#ajout de balises # et @
+sed -i "s/#.\w*/<span class="lien">&<\/span>/g" tweets.html
+sed -i "s/@.\w*/<span class="ate">&<\/span>/g" tweets.html
