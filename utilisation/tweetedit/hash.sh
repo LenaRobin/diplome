3dc036cc1883c524093f0a6a8b79bfee64f1f2e8
mathieu <mathieu@T40.pouet>
2015-05-31 23:08:30 +0200

    dec-feb

--- utilisation/tweetedit/hash.sh
+++ utilisation/tweetedit/hash.sh
@@ -3,5 +3,5 @@ sed -i 's/twitter_user">\t"/twitter_user">/g' tweets.html
 sed -i 's/\."/\./g' tweets.html
 
 #ajout de balises # et @
-sed -i "s/#.\w*/<span class="lien">&<\/span>/g" tweets.html
-sed -i "s/@.\w*/<span class="ate">&<\/span>/g" tweets.html
+sed -i "s/#.\w*/<span class=\"lien\">&<\/span>/g" tweets.html
+sed -i "s/@.\w*/<span class=\"ate\">&<\/span>/g" tweets.html
